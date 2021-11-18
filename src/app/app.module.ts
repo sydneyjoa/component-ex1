6a35d73a4756acf4274d575b89416de00c2fd3c1
sydneyjoa <SYDNEY.JOA@newhorizons.edu.do>
2021-11-18 17:54:43 +0000

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TableComponent } from './table/table.component';
+import { ButtonComponent } from './button/button.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TableComponent,
+    ButtonComponent
   ],
   imports: [
     BrowserModule,
